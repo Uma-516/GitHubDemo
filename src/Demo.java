8e22161137ccaa39a9a0fd6de08d4825d543fd46
Kishore <kishorebabu.ganta@gmail.com>
2023-05-27 21:57:27 +0530

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Demo {
+
+    void m1(){
+        System.out.println("This is m1 method");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Test {
+    void m2(){
+        System.out.println("This is from m2 method");
+    }
+}
