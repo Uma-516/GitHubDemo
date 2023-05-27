e5be7c90181870593825cb70567c1afa9c4d1c9b
Kishore <kishorebabu.ganta@gmail.com>
2023-05-28 00:19:40 +0530

    m1 and m2 methods added

--- Employee.java
+++ Employee.java
@@ -1,5 +1,13 @@
 public class Employee {
 
+    void m1(){
+        System.out.println("This is m1 method");
+    }
+
+    void m2(){
+        System.out.println("This is m2 method");
+    }
+
     void m3(){
         System.out.println("This is m3 method");
     }
